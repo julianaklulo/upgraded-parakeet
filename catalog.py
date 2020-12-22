ca4ed5d03da1e61e88886a4ca28c2894d168aefe
Juliana Karoline <julianaklulo@gmail.com>
2020-12-22 17:58:20 -0300

    Create catalog listing functions

---
+++ catalog.py
@@ -0,0 +1,12 @@
+marketplaces = ['Mercado Livre', 'B2W', 'Magazine Luiza', 'Madeira Madeira']
+categories = ['Móveis', 'Decoração', 'Eletrônicos', 'Eletrodomésticos']
+subcategories = ['Sofás', 'Celulares']
+
+def list_marketplaces():
+    return marketplaces
+
+def list_categories():
+    return categories
+
+def list_subcategories():
+    return subcategories
