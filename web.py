fb7bd3c5eb0784c58f439ba6dee0d34fe4340537
Juliana Karoline <julianaklulo@gmail.com>
2020-12-23 16:13:30 -0300

    Change routes to render templates

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from catalog import list_marketplaces, list_categories, list_subcategories
 
@@ -7,64 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """
-    <h1>Olist Catalog</h1>
-    <ol>
-        <li><a href='/marketplaces'>List all marketplaces</a>
-        <li><a href='/categories'>List marketplace's categories</a>
-        <li><a href='/subcategories'>List category's subcategories</a>
-    </ol>
-    """
+    return render_template('index.html')
 
 @app.route('/marketplaces')
 def marketplaces():
     marketplaces = list_marketplaces()
-    response = f"""
-    <h1>Olist Catalog</h1>
-    <h3>Marketplaces:</h3>
-    <ol>
-    """
-    for marketplace in marketplaces:
-        response += f"<li>{marketplace}</li>"
-    response += """
-    </ol>
-    <a href='/'>Go Back</a> 
-    """
-
-    return response
+    return render_template('marketplaces.html', marketplaces=marketplaces)
 
 @app.route('/categories')
 def categories():
     categories = list_categories()
-    response = f"""
-    <h1>Olist Catalog</h1>
-    <h3>Categories:</h3>
-    <ol>
-    """
-    for category in categories:
-        response += f"<li>{category}</li>"
-    response += """
-    </ol>
-    <a href='/'>Go Back</a> 
-    """
-
-    return response
+    return render_template('categories.html', categories=categories)
 
 @app.route('/subcategories')
 def subcategories():
     subcategories = list_subcategories()
-    response = f"""
-    <h1>Olist Catalog</h1>
-    <h3>Subcategories:</h3>
-    <ol>
-    """
-    for subcategory in subcategories:
-        response += f"<li>{subcategory}</li>"
-    response += """
-    </ol>
-    <a href='/'>Go Back</a> 
-    """
-
-    return response
+    return render_template('subcategories.html', subcategories=subcategories)
 
 app.run(debug=True)
