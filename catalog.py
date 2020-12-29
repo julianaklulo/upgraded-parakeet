9ea8abbdea4049595d543313d37770488ca9519c
Juliana Karoline <julianaklulo@gmail.com>
2020-12-29 00:14:12 -0300

    Add logging into catalog listing functions

--- catalog.py
+++ catalog.py
@@ -6,12 +6,15 @@ marketplaces_dict = read_marketplaces()
 
 def list_marketplaces():
     marketplaces = [key for key in marketplaces_dict.keys()]
+    write_log("List marketplaces")
     return marketplaces
 
 def list_categories(marketplace_name):
     categories = marketplaces_dict[marketplace_name]
+    write_log(f"List categories of marketplace {marketplace_name}")
     return categories
 
 def list_subcategories(category_name):
     subcategories = categories_dict[category_name]
+    write_log(f"List subcategories of category {category_name}")
     return subcategories
