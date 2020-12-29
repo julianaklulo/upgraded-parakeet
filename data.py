bc4358a2a81648d873ce189f8339991598318b63
Juliana Karoline <julianaklulo@gmail.com>
2020-12-28 23:14:23 -0300

    Add functions to persist data to text files

---
+++ data.py
@@ -0,0 +1,31 @@
+def read_categories():
+    categories_dict = {}
+    with open('categories.txt', 'r') as categories_file:
+        for line in categories_file:
+            line = line.strip()
+            category = line.split(';')[0]
+            subcategories = [subcategory for subcategory in line.split(';')[1:]]
+            categories_dict[category] = subcategories
+    return categories_dict
+
+def write_categories(category_dict):
+    category = list(category_dict.keys())[0]
+    subcategories = ';'.join(category_dict[category])
+    with open('categories.txt', 'a') as categories_file:
+        categories_file.write(category + ';' + subcategories + '\n')
+
+def read_marketplaces():
+    marketplaces_dict = {}
+    with open('marketplaces.txt', 'r') as marketplaces_file:
+        for line in marketplaces_file:
+            line = line.strip()
+            marketplace = line.split(';')[0]
+            categories = [category for category in line.split(';')[1:]]
+            marketplaces_dict[marketplace] = categories
+    return marketplaces_dict
+
+def write_marketplaces(marketplace_dict):
+    marketplace = list(marketplace_dict.keys())[0]
+    categories = ';'.join(marketplace_dict[marketplace])
+    with open('marketplaces.txt', 'a') as marketplaces_file:
+        marketplaces_file.write(marketplace + ';' + categories + '\n')
