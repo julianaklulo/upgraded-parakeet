c1673b18f8b3c885566af3341a7226d5394dcb05
Juliana Karoline <julianaklulo@gmail.com>
2020-12-30 16:29:31 -0300

    Add create marketplace route

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-from catalog import list_marketplaces, list_categories, list_subcategories
+from catalog import list_marketplaces, list_categories, list_categories_of_marketplace, list_subcategories, add_marketplace
 
 
 app = Flask(__name__)
@@ -14,9 +14,21 @@ def marketplaces():
     marketplaces = list_marketplaces()
     return render_template('marketplaces.html', marketplaces=marketplaces)
 
+@app.route('/create_marketplace')
+def create_marketplace():
+    categories = list_categories()
+    return render_template('create_marketplace.html', categories=categories)
+
+@app.route('/marketplace')
+def marketplace():
+    marketplace_name = request.args.get('marketplace_name')
+    categories = [category for category in request.args.keys() if category != 'marketplace_name']
+    add_marketplace(marketplace_name, categories)
+    return render_template('success_message.html', message="Marketplace created successfully!")
+
 @app.route('/<marketplace>/categories')
 def categories(marketplace):
-    categories = list_categories(marketplace)
+    categories = list_categories_of_marketplace(marketplace)
     return render_template('categories.html', marketplace=marketplace, categories=categories)
 
 @app.route('/<marketplace>/<category>/subcategories')
